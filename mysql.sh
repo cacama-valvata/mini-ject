2e06bb093aa1154bfc8c07c0f9652ea86927f629
Casey Colley <code@caseycolley.com>
2023-05-17 02:23:45 -0700

    updated docker script

--- mysql.sh
+++ mysql.sh
@@ -1,2 +1,1 @@
-docker run -d --name mini -e MYSQL_RANDOM_ROOT_PASSWORD=yes -e MYSQL_USER=mini -e MYSQL_PASSWORD=wL9y4pDF4f8MSv -e MYSQL_DATABASE=mini mysql:latest
-
+docker run -d --name mini -e MYSQL_RANDOM_ROOT_PASSWORD=yes -e MYSQL_USER=mini -e MYSQL_PASSWORD=wL9y4pDF4f8MSv -e MYSQL_DATABASE=mini -v ./mysql:/data -p 3307:3306 mysql:latest
